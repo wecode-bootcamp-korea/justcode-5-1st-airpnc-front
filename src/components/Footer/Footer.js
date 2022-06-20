61b78a3ac319610e27f528d08d0f7a3c9564269c
최정훈 <jeongssi94@gmail.com>
2022-06-20 16:37:54 +0900

    test jh

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import css from './Footer.module.scss';
 
 function Footer() {
-  return <div className={css.container}></div>;
+  return <div className={css.container}>최정훈</div>;
 }
 
 export default Footer;
