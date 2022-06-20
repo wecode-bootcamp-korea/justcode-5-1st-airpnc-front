66d116e0bf036d4c438658f0e191cf084dc8b102
김민욱 <minuk3508@gmail.com>
2022-06-20 17:22:09 +0900

    Router 설정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,12 @@ import css from './Header.module.scss';
 function Header() {
   return(
   <div className={css.container}>
-
+    <div className={css.logo}></div>
+    <div className={css.searchBar}></div>
+    <div className={css.user}></div>
   </div>
-);
+  );
+
 }
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import css from './Home.module.scss';
 
 function Home() {
-  return <div className={css.container}></div>;
+  return <div className={css.container}>안녕하세요</div>;
 }
 
 export default Home;
