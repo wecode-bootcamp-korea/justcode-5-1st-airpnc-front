17e4cb433dd9282823a3a7ca3f55e9f9889e39d1
최정훈 <jeongssi94@gmail.com>
2022-06-20 16:48:59 +0900

    최정훈 브런치 생성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import css from './Footer.module.scss';
 
 function Footer() {
-  return <div className={css.container}>최정훈</div>;
+  return <div className={css.container}></div>;
 }
 
 export default Footer;
