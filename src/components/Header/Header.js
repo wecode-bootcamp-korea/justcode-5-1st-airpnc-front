25e138dd4291c641262feedd91a9d9ee6f47e368
최정훈 <jeongssi94@gmail.com>
2022-06-21 11:34:05 +0900

    Add: header.js기능구현

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import css from './Header.module.scss';
 
+import { GiMushroomHouse } from 'react-icons/gi';
+import { FaUserCircle, FaSearch } from 'react-icons/fa';
+import { GrLanguage } from 'react-icons/gr';
+import { VscThreeBars } from 'react-icons/vsc';
 function Header() {
-  return (
-    <div className={css.container}></div>
-  );
+  return <div className={css.container}></div>;
 }
 
 export default Header;
