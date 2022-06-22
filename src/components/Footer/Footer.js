904122f49b7b86a908b07cdf2050544197ae1b3f
azure928 <ji@DESKTOP-UBR9OBO.localdomain>
2022-06-22 17:35:02 +0900

    footer 브랜치 생성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 import css from './Footer.module.scss';
 
 function Footer() {
-  return <div className={css.container}></div>;
+  return (
+    <div className={css.container}>
+      <h1>푸터입니다!!</h1>
+    </div>
+  );
 }
 
 export default Footer;
